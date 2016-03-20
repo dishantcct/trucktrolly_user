bfe01d472bdd5886433f3017bfbcefdb59760252
Umesh Prajapati <uprajapatik@gmail.com>
2016-03-20 23:39:01 +0530

    init commit for development

---
+++ app/src/main/java/com/trucktrollyuser/SignUpActivity.java
@@ -0,0 +1,27 @@
+package com.trucktrollyuser;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+/*
+ * Created by Umesh Prajapati on 20-03-2016.
+ */
+public class SignUpActivity extends AppCompatActivity {
+
+    private EditText edtTextName;
+    private EditText edtTextEmail;
+    private EditText edtTextMobileNumber;
+    private EditText edtTextPassword;
+    private EditText edtTextConfirmPassword;
+    private Button   btnSubmit;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_screen);
+    }
+}
